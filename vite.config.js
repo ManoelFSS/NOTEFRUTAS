dd8cd52092d8c94299348686e2b858c633e1c97f
manoel <piloukos45@gmail.com>
2025-07-08 22:02:17 -0300

    fix: styles

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         name: 'Seu Nome do App',
         short_name: 'NomeApp',
         description: 'Descrição do seu app',
-        theme_color: '#ffffff',
+        theme_color: ' #FE7E01',
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
